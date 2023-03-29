39d126db63cc16c02b120705dd686a5b33a9031d
Bex Martin <rebecca.martin@adlsmartcare.com>
2023-03-29 14:31:43 +0100

    Replaced links to dboard with index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,10 +1,9 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200">
-            {{ __('Pick something') }}
+            {{ __('Movie Picker') }}
         </h2>
     </x-slot>
-
 
     <div class="flex justify-center sm:my-8">
         <div class="sm:flex sm:basis-full lg:basis-3/4 xl:basis-1/2">
